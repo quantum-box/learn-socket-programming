b745fb1f2777521648b3b02faba06502fc3808d4
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2023-09-12 10:46:05 +0900

    1.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::env;
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port].");
+        std::process::exit(1);
+    }
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address = &args[3];
+    match protocol {
+        "tpc" => match role {
+            "server" => {
+                // TODO: TCPサーバーを実装
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
+            }
+            "client" => {
+                // TODO: TCPクライアントを実装
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                // TODO: UDPサーバーを実装
+            }
+            "client" => {
+                // TODO: UDPクライアントを実装
+            }
+            _ => {
+                missing_role();
+            }
+        },
+        _ => {
+            error!("Please specify tpc or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
+}
+
+/// 第二引数が不正な時にエラーを出力する
+fn missing_role() {
+    error!("Please specify server or client on the 2nd argument.");
+    std::process::exit(1);
 }
